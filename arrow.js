8cec5a07b7799450cb5f675c384c2c50ea1cc87a
AdamZullowa06 <adamzullowa06@gmail.com>
2021-07-18 00:07:21 +0700

    new file arrow

---
+++ arrow.js
@@ -0,0 +1,45 @@
+// function expression
+
+const tampilNama = function (nama) {
+    return `Halo ${nama}`;
+}
+
+console.log(tampilNama('John Doe'));
+
+// function arrow
+const helloWorld = (nama) => { return `Hello ${nama}`;}
+console.log(helloWorld('World'));
+
+// function arrow lebih dari satu parameter
+const index = (nama, waktu) => {
+    return `Selamat ${waktu}, ${nama}`;
+}
+
+console.log(index('John', 'malam'));
+
+// implisit return
+const index2 = nama => `Halo ${nama}`;
+console.log(index2('Adam'));
+
+// tanpa parameter
+const index3 = () => `Hello world`;
+console.log(index3());
+
+// contoh kasus
+// menampilkan jumlah karakter dari masing-masing index
+let mahasiswa = ['Adam', 'Ravi', 'Bagus'];
+
+// cara function expression
+// let jumlahHuruf = mahasiswa.map(function (nama) {
+//     return nama.length;
+// })
+// console.log(jumlahHuruf);
+
+// cara arrow function
+// dalam bentuk array
+// let jumlahHuruf = mahasiswa.map(nama => nama.length);
+// console.log(jumlahHuruf);
+
+// arrow function return dalam bentuk object
+let jumlahHuruf = mahasiswa.map(nama => ({ nama, jmlhHuruf: nama.length }));
+console.table(jumlahHuruf);
